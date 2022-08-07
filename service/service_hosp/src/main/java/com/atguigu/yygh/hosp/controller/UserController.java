ce4536e4b3a2c1769fe26f9671cad51aa3595414
piscesliu <piscesliu66@qq.com>
2022-08-07 22:00:22 +0800

    添加用户接口，用于前端测试用户登录

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.atguigu.yygh.hosp.controller;
+
+import com.atguigu.yygh.common.config.result.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+@CrossOrigin
+public class UserController {
+
+    @PostMapping("/login")
+    public R login() {
+        return R.ok().data("token", "admin-token");
+    }
+
+    @GetMapping("/info")
+    public R info(String token) {
+        log.info("token: {}", token);
+        return R.ok()
+                .data("roles", "[admin]")
+                .data("introduction", "admin")
+                .data("avatar", "https://pic4.zhimg.com/80/v2-9715174c95f9665b71cc8f3cceea910f_720w.jpg")
+                .data("name", "Super admin");
+    }
+}
