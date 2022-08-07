f9a686d9da773ca334a18c657fb18379292373cb
piscesliu <piscesliu66@qq.com>
2022-08-07 13:51:33 +0800

    添加删除医院接口 /remove/{id}

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -5,10 +5,9 @@ import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiParam;
+import io.swagger.models.auth.In;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -34,5 +33,12 @@ public class HospitalSetController {
     public List<HospitalSet> findAll() {
         return hospitalSetService.list();
     }
+
+    @ApiOperation(value = "根据医院 id 删除医院信息")
+    @DeleteMapping("/remove/{id}")
+    public boolean removeById(@ApiParam(name = "id", value = "医院设置ID", required = true) @PathVariable("id") String id) {
+        return hospitalSetService.removeById(id);
+    }
+
 }
 
