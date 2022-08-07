fe05b10238a5a955d34fcceac089569d5e50b9c1
piscesliu <piscesliu66@qq.com>
2022-08-07 16:12:07 +0800

    根据医院名称获取医院分页信息

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -4,11 +4,13 @@ package com.atguigu.yygh.hosp.controller;
 import com.atguigu.yygh.common.config.result.R;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.models.auth.In;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -58,6 +60,35 @@ public class HospitalSetController {
     public R pageList(@PathVariable("current") Integer current, @PathVariable("limit") Integer limit) {
         Page<HospitalSet> page = new Page<>(current, limit);
         hospitalSetService.page(page);
+        return R.ok().data("total", page.getTotal()).data("rows", page.getRecords());
+    }
+
+    @ApiOperation(value = "根据医院名称获取医院分页信息")
+    @PostMapping("/{current}/{limit}")
+    public R pageQuery(
+            @ApiParam(name = "current", value = "当前页码", required = true)
+            @PathVariable("current") Integer current,
+
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable("limit") Integer limit,
+
+            @ApiParam(name = "hospitalSetQueryVo", value = "查询对象")
+            @RequestBody HospitalSetQueryVo hospitalSetQueryVo) {
+
+        Page<HospitalSet> page = new Page<>(current, limit);
+
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+        if (hospitalSetQueryVo != null) {
+            String hosname = hospitalSetQueryVo.getHosname();
+            String hoscode = hospitalSetQueryVo.getHoscode();
+            if (!StringUtils.isEmpty(hosname)) {
+                queryWrapper.like("hosname", hosname);
+            }
+            if (!StringUtils.isEmpty(hoscode)) {
+                queryWrapper.eq("hoscode", hoscode);
+            }
+        }
+        hospitalSetService.page(page, queryWrapper);
         return R.ok().data("total", page.getTotal()).data("rows", page.getRecords());
     }
 
