b7af243a954b466f79750214fbf84776c5855f97
piscesliu <piscesliu66@qq.com>
2022-08-07 13:41:15 +0800

    整合 swagger2

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -3,10 +3,12 @@ package com.atguigu.yygh.hosp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 // 扫描到 mapper 层
 @MapperScan("com.atguigu.yygh.hosp.mapper")
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.atguigu"})
 public class ServiceHospApplication {
 
     public static void main(String[] args) {
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -3,6 +3,8 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +21,7 @@ import java.util.List;
  * @author Liu-Liu
  * @since 2022-08-07
  */
+@Api(tags = "预约设置接口")
 @RestController
 @RequestMapping("/hosp/hospital-set")
 public class HospitalSetController {
@@ -26,6 +29,7 @@ public class HospitalSetController {
     @Resource
     private HospitalSetService hospitalSetService;
 
+    @ApiOperation(value = "查询所有医院信息")
     @GetMapping("/findAll")
     public List<HospitalSet> findAll() {
         return hospitalSetService.list();
