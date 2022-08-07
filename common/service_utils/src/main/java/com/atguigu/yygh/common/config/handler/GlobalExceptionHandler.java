cb6f900d7889eb74927f8e21116879c0458e2a4f
piscesliu <piscesliu66@qq.com>
2022-08-07 20:04:37 +0800

    添加统一日志处理

--- common/service_utils/src/main/java/com/atguigu/yygh/common/config/handler/GlobalExceptionHandler.java
+++ common/service_utils/src/main/java/com/atguigu/yygh/common/config/handler/GlobalExceptionHandler.java
@@ -2,30 +2,32 @@ package com.atguigu.yygh.common.config.handler;
 
 import com.atguigu.yygh.common.config.exception.YyghException;
 import com.atguigu.yygh.common.config.result.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // 全局统一异常处理
     @ExceptionHandler(Exception.class)
     public R error(Exception exception) {
-        exception.printStackTrace();
+        log.error("yygh has error{}", exception.getMessage());
         return R.error().message("发生了全局异常");
     }
 
     // 处理特定异常
     @ExceptionHandler(ArithmeticException.class)
-    public R error(ArithmeticException arithmeticException) {
-        arithmeticException.printStackTrace();
+    public R error(ArithmeticException ex) {
+        log.error("yygh has error{}", ex.getMessage());
         return R.error().message("数学逻辑异常");
     }
 
     // 处理自定义异常
     @ExceptionHandler(YyghException.class)
     public R error(YyghException ex) {
-        ex.printStackTrace();
+        log.error("yygh has error{}", ex.getMessage());
         return R.error().code(ex.getCode()).message(ex.getMessage());
     }
 }
