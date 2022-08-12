1082ab78ba345c4989a8398fbde8faeb192a604e
piscesliu <piscesliu66@qq.com>
2022-08-12 19:48:27 +0800

    添加 README.md

--- service_gateway/src/main/java/com/atguigu/yygh/gateway/ApiGatewayApplication.java
+++ service_gateway/src/main/java/com/atguigu/yygh/gateway/ApiGatewayApplication.java
@@ -3,11 +3,9 @@ package com.atguigu.yygh.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-// @ComponentScan(basePackages = {"com.atguigu"})
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
